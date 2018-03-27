76c4cd1dc00ac1058a6318f5e90fab87d00a5c69
Mujib517 <Mujib517@gmail.com>
2018-03-27 14:04:57 +0530

    Mocha test setup

---
+++ tests/true-logger.spec.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe("True logger", function () {
+
+    it('should expect true to be true', function () {
+        expect(true).to.equal(true);
+    });
+})
