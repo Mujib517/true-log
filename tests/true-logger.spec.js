8ad6e0f7115b250a7f84863a45bec6b03bc5ff3c
Mujib517 <Mujib517@gmail.com>
2018-03-27 14:48:01 +0530

    Failing test to fail build

--- tests/true-logger.spec.js
+++ tests/true-logger.spec.js
@@ -5,4 +5,8 @@ describe("True logger", function () {
     it('should expect true to be true', function () {
         expect(true).to.equal(true);
     });
+
+    it('a failing test', function () {
+        expect(false).to.equal(true);
+    })
 })
